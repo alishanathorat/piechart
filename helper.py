2f7345617053601af9278910af9526cc4e52ad88
alishanathorat <140586408+alishanathorat@users.noreply.github.com>
2024-10-01 13:48:34 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,13 @@
+import emoji 
+
+def emoji_helper(selected_user,df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if c in emoji.UNICODE_EMOJI['en']])
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+
+    return emoji_df
